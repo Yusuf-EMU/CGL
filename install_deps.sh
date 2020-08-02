7fcb2afef01171eae74d1d877991215225a1f432
Th3T3chn0G1t <44269608+Th3T3chn0G1t@users.noreply.github.com>
2020-08-02 17:57:28 +0100

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -2,22 +2,8 @@
 
 sudo apt-get update
 
-# GLEW Install
-    sudo apt-get -y install libglew-dev
-
-# Compiling and installing GLFW to avoid weirdness on some distros
-    sudo apt-get -y install libx11-dev
-    sudo apt-get -y install libxi-dev
-    sudo apt-get -y install libxrandr-dev
-    sudo apt-get -y install libxinerama-dev
-    sudo apt-get -y install libxcursor-dev
-    git clone https://github.com/glfw/glfw
-    cd glfw
-    cmake .
-    make
-    sudo make install
-    cd ..
-    rm -rf glfw
+# GLEW & GLFW
+    sudo apt-get -y install libglew-dev libglfw-dev
 
 # CGLM
     git clone https://github.com/recp/cglm
