6639e332868b109aeac5dd626724fa575ec78ae5
Th3T3chn0G1t <bbanerjee@stmc.ie>
2020-08-03 10:13:15 +0100

    Adding the basic prerequisites for including libs

--- install_deps.sh
+++ install_deps.sh
@@ -2,8 +2,25 @@
 
 sudo apt-get update
 
-# GLEW & GLFW
-    sudo apt-get -y install libglew-dev libglfw-dev
+# GLEW
+    git clone https://github.com/nigels-com/glew
+    cd glew
+    cd auto
+    make
+    cd ..
+    make
+    make install
+    cd ..
+    rm -rf GLEW
+
+# GLFW
+    git clone https://github.com/glfw/glfw
+    cd glfw
+    cmake .
+    make
+    make install
+    cd ..
+    rm -rf cglm
 
 # CGLM
     git clone https://github.com/recp/cglm
